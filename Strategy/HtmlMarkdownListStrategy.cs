a04963e8c382750151c6239c6f745fcede36275e
Pablo Casale <casalepablo83@gmail.com>
2022-02-21 18:44:31 -0300

    Strategy Pattern Dynamic

---
+++ Strategy/HtmlMarkdownListStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class HtmlMarkdownListStrategy : IListStrategy
+    {
+        public void Start(StringBuilder sb)
+        {
+        }
+
+        public void End(StringBuilder sb)
+        {
+        }
+
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($" * {item}");
+        }
+    }
+}
