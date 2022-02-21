05f27d246a26a3a02327501f68981f91ae207c98
Pablo Casale <casalepablo83@gmail.com>
2022-02-21 17:31:38 -0300

    Proxy Virtual Pattern

---
+++ Proxy/ProxyApp/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.ProxyApp
+{
+    public class CustomerRepository : IRepository
+    {
+        private IList<Customer> customers;
+
+        public CustomerRepository()
+        {
+            customers = new List<Customer>();
+        }
+
+        public IList<Customer> Get()
+        {
+            return customers;
+        }
+
+        public void Save(Customer customer)
+        {
+            customers.Add(customer);
+        }
+    }
+}
---
+++ Proxy/VirtualProxy/CustomerRepositoryProxy.cs
@@ -0,0 +1,48 @@
+﻿using Proxy.ProxyApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.VirtualProxy
+{
+    public class CustomerRepositoryProxy : IRepository
+    {
+        private IRepository proxyApp;
+
+        public IList<Customer> Get()
+        {
+            if (proxyApp == null)
+            {
+                proxyApp = new CustomerRepository();
+            }
+
+            if (Session.CanGetAll)
+            {
+                return proxyApp.Get();
+            }
+            else
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+
+        public void Save(Customer customer)
+        {
+            if (proxyApp == null)
+            {
+                proxyApp = new CustomerRepository();
+            }
+
+            if (Session.CanSave)
+            {
+                proxyApp.Save(customer);
+            }
+            else
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+    }
+}
