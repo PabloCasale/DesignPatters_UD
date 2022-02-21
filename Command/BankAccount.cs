34fa8631b2dce28c5befcba80864e7d492ca46c8
Pablo Casale <casalepablo83@gmail.com>
2022-02-21 16:25:24 -0300

    Command pattern

---
+++ Command/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class BankAccount
+    {
+        private int balance;
+        private int overdraftLimit = -500;
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited: ${amount} - Balance is now: {balance}");
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (balance - amount >= overdraftLimit)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrew ${amount} - Balance is now: {balance}");
+                return false;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+}
---
+++ Command/BankAccountCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class BankAccountCommand : ICommand
+    {
+        private BankAccount account;
+        private Action action;
+        private int amount;
+        private bool succeded;
+
+        public BankAccountCommand(BankAccount account, Action action, int amount)
+        {
+            this.account = account;
+            this.action = action;
+            this.amount = amount;
+        }
+
+        public void Call()
+        {
+            switch (action)
+            {
+                case Action.Deposit:
+                    account.Deposit(amount);
+                    succeded = true;
+                    break;
+
+                case Action.Withdraw:
+                    succeded = account.Withdraw(amount);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public void Undo()
+        {
+            if (!succeded)
+            {
+                return;
+            }
+
+            switch (action)
+            {
+                case Action.Deposit:
+                    account.Withdraw(amount);
+                    break;
+
+                case Action.Withdraw:
+                    account.Deposit(amount);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public enum Action
+        {
+            Deposit,
+            Withdraw
+        }
+    }
+}
